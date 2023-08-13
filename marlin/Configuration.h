99f3dec83d6fb540dc49bb8b590f694546fc1695
Jason Kölker <jason@koelker.net>
2023-08-13 01:42:29 +0000

    feat(marlin): reduce microstepping to 64

--- marlin/Configuration.h
+++ marlin/Configuration.h
@@ -1170,7 +1170,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 1280, 1280, 40, 4.44, 4.44, 40 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 40, 4.44, 4.44, 40 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
